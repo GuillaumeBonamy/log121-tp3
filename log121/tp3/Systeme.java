f66a496fac9ae35aa3556bb8f3c66e730c975e09
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-02 04:48:27 +0000

    La fameuse classe système

---
+++ log121/tp3/Systeme.java
@@ -0,0 +1,17 @@
+
+public class Systeme {
+	
+	/**
+	 * Methode permettant de creer une nouvelle fenetre
+	 */
+	public static void creerFenetre()
+	{
+		new Fenetre();
+	}
+	
+	public static void main(String [] args)
+	{
+		creerFenetre();
+	}
+	
+}
