f21a0db1f53b78f8b6156ee2cb9dbc92f8808cd7
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-02 04:46:39 +0000

    Ajout de la fenetre, en attente de commande pour pouvoir faire le actionPerformed

--- log121/tp3/controleur/VueImageModifie.java
+++ log121/tp3/controleur/VueImageModifie.java
@@ -1,16 +1,88 @@
-package log121.tp3.controleur;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
-public class VueImageModifie extends JFrame implements Observer {
 
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
+public class VueImageModifie extends JFrame implements ActionListener, Observer{
+
+	private JMenuBar menuBar;
+	private JMenu menuAction;
+	private JMenuItem translate;
+	private JMenuItem zoom;
+	private JPanel panelImage;
+	private JLabel labelImage;
+	
+	/**
+	 * Constructeur de la classe VueImageModifie
+	 * @param img L'image a affiche dans la fenetre
+	 */
+	public VueImageModifie(Image img)
+	{
+		setTitle("Vue image modifie");
+		setLocation(450,450);
+		setSize(400,400);
 		
+		creerMenu();
+		
+		this.panelImage=new JPanel();
+		this.labelImage=new JLabel(new ImageIcon(img.getCheminImage()));
+		
+		this.panelImage.add(labelImage);
+		
+		this.add(panelImage);
+		
+		
+		
+		setVisible(true);
+	}
+	
+	/**
+	 * Methode permettant de creer le menu de la fenetre
+	 */
+	public void creerMenu()
+	{
+		menuBar = new JMenuBar();
+		
+		menuAction=new JMenu("Action");
+		translate=new JMenuItem("Translater");
+		zoom=new JMenuItem("Zoomer");
+		
+		menuAction.add(translate);
+		translate.addActionListener(this);
+		
+		menuAction.add(zoom);
+		zoom.addActionListener(this);
+		
+		menuBar.add(menuAction);
+		
+		this.setJMenuBar(menuBar);
 	}
 
+	/**
+	 * Methode permettant de gerer les actions sur les items
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==translate)
+		{
+			
+		}
+		else if(e.getSource()==zoom)
+		{
+			
+		}
+	}
+	
+	 public void update(Observable arg0, Object arg1) {
+         // TODO Auto-generated method stub
+         
+	 }
 }
