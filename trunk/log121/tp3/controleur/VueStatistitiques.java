b65c8c20f433fed899282819ec79124cfcdfed8a
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-02 04:47:32 +0000

    Ajout de la fenetre, et je suis actuellement entrain de chercher comment faire une barre avec un curseur a bouger pour le zoom

--- trunk/log121/tp3/controleur/VueStatistitiques.java
+++ trunk/log121/tp3/controleur/VueStatistitiques.java
@@ -1,16 +1,41 @@
-package log121.tp3.controleur;
-
+import java.awt.GridLayout;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class VueStatistitiques extends JFrame implements Observer {
 
-	@Override
-	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+public class VueStatistiques extends JFrame implements Observer {
+	
+	private JPanel panel;
+	private JTextField tf;
+	
+	/**
+	 * Constructeur de la classe VueStatistiques
+	 * @param img L'image dont on doit afficher les statistiques
+	 */
+	public VueStatistiques(Image img)
+	{
+		setTitle("Vue statistique");
+		setLocation(850,450);
+		setSize(200,200);
+	
+		panel=new JPanel(new GridLayout(1,2));
+		tf=new JTextField("x1:"+img.getX1()+ " x2: "+img.getX2()+"\ny1: "+img.getY1()+" y2: "+img.getY2());
+		panel.add(tf);
 		
+		this.add(panel);
+		
+		setVisible(true);
 	}
-
+	
+	/**
+	 * 
+	 */
+	 public void update(Observable arg0, Object arg1) {
+         // TODO Auto-generated method stub
+         
+	 }
 }
