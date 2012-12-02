bf5259021cf647f2dfc820ad44375cbf7ce042e1
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-02 04:49:16 +0000

    classe origine

---
+++ log121/tp3/Origine.java
@@ -0,0 +1,28 @@
+
+public class Origine {
+
+		private Image state;
+	
+		/**
+		 * Constructeur de la classe Origine
+		 */
+		public Origine() {}
+		
+		/**
+		 * Methode permettant de creer un nouveau MementoImage
+		 * @return le MementoImage creer
+		 */
+		public MementoImage createMementoImage()
+		{
+			return new MementoImage(state);
+		}
+		
+		/**
+		 * @param state La nouvelle image
+		 */
+		public void setState(Image state)
+		{
+			this.state=state;
+		}
+		
+}
