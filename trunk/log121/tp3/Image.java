8d59db5eeebd3012a95d5ab3b0bdb8e2b0938bed
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-02 04:49:58 +0000

    la classe image

---
+++ trunk/log121/tp3/Image.java
@@ -0,0 +1,61 @@
+
+public class Image {
+
+	private int x1, x2, y1, y2;
+	private String cheminImage;
+	
+	/**
+	 * Constructeur de la classe Image
+	 * @param cheminImage L'emplacement de l'image
+	 */
+	public Image(String cheminImage)
+	{
+		this.cheminImage=cheminImage;
+	}
+	
+	
+	/**
+	 * @return L'emplacement de l'image
+	 */
+	public String getCheminImage() 	{return cheminImage;}
+
+	/** @return La coordonnee x1 de l'image*/
+	public int getX1() {return x1;}
+	/** @return La coordonnee y2 de l'image*/
+	public int getY2() {return y2;}
+	/** @return La coordonnee x2 de l'image*/
+	public int getX2() {return x2;}
+	/** @return La coordonnee y1 de l'image*/
+	public int getY1() {return y1;}
+	
+
+	/**
+	 * Modifie la coordonnee x1 de l'image
+	 * @param x1 La nouvelle coordonnee
+	 */
+	public void setX1(int x1) {this.x1 = x1;}
+	/**
+	 * Modifie la coordonnee x2 de l'image
+	 * @param x2 La nouvelle coordonnee
+	 */
+	public void setX2(int x2) {this.x2 = x2;}
+	/**
+	 * Modifie la coordonnee y1 de l'image
+	 * @param y1 La nouvelle coordonnee
+	 */
+	public void setY1(int y1) {this.y1 = y1;}
+	/**
+	 * Modifie la coordonnee y2 de l'image
+	 * @param y2 La nouvelle coordonnee
+	 */
+	public void setY2(int y2) {this.y2 = y2;}
+	
+
+	
+	
+	
+
+	
+	
+	
+}
