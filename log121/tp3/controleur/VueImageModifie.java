b9457b17843dadac1d79bf11f502ce44f3c80af4
james.medvedovski@gmail.com <james.medvedovski@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-05 23:18:10 +0000

    Zoom par souris. Translation par souris.
    -> Il faut ajouter une protection pour assurer que la valeur de x et y d'une image ne soit pas inférieur à 0 et ne soit pas supérieure à la grandeur de l'image initiale (Receveur.java).

--- log121/tp3/controleur/VueImageModifie.java
+++ log121/tp3/controleur/VueImageModifie.java
@@ -3,6 +3,11 @@ package log121.tp3.controleur;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.util.Observable;
 import java.util.Observer;
@@ -17,7 +22,8 @@ import javax.swing.JPanel;
 
 import log121.tp3.Image;
 
-public class VueImageModifie extends JFrame implements ActionListener, Observer {
+public class VueImageModifie extends JFrame implements ActionListener,
+		MouseWheelListener, MouseMotionListener, MouseListener, Observer {
 
 	private JMenuBar menuBar;
 	private JMenu menuAction;
@@ -70,6 +76,9 @@ public class VueImageModifie extends JFrame implements ActionListener, Observer 
 
 		this.panelImage = new JPanel();
 		this.labelImage = new JLabel(dessinerIconeImage());
+		labelImage.addMouseWheelListener(this);
+		labelImage.addMouseMotionListener(this);
+		labelImage.addMouseListener(this);
 
 		this.panelImage.add(labelImage);
 
@@ -120,7 +129,7 @@ public class VueImageModifie extends JFrame implements ActionListener, Observer 
 		/*
 		 * Ce code est temporaire. Il faut passer par GestionCommande.
 		 */
-		
+
 		if (e.getSource() == translationBas) {
 			// test
 			Image iClone = img.getclone();
@@ -169,6 +178,86 @@ public class VueImageModifie extends JFrame implements ActionListener, Observer 
 			// test
 		}
 	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		if (e.getWheelRotation() < 0) {
+			// test
+			Image iClone = img.getclone();
+			iClone.setX1(img.getX1() + 25);
+			iClone.setX2(img.getX2() - 25);
+			iClone.setY1(img.getY1() + 25);
+			iClone.setY2(img.getY2() - 25);
+			Controleur.getInstance().tempSetImage(iClone);
+			// test
+		} else {
+			// test
+			Image iClone = img.getclone();
+			iClone.setX1(img.getX1() - 25);
+			iClone.setX2(img.getX2() + 25);
+			iClone.setY1(img.getY1() - 25);
+			iClone.setY2(img.getY2() + 25);
+			Controleur.getInstance().tempSetImage(iClone);
+			// test
+		}
+	}
+	
+	// TEST
+	int clickX;
+	int clickY;
+	
+
+	@Override
+	public void mouseDragged(MouseEvent e) {		
+		// test
+		Image iClone = img.getclone();
+		iClone.setX1(img.getX1() - (e.getX() - clickX));
+		iClone.setX2(img.getX2() - (e.getX() - clickX));
+		iClone.setY1(img.getY1() - (e.getY() - clickY));
+		iClone.setY2(img.getY2() - (e.getY() - clickY));
+		Controleur.getInstance().tempSetImage(iClone);
+		clickX = e.getX();
+		clickY = e.getY();
+		// test
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		clickX = e.getX();
+		clickY = e.getY();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// TEST
 
 	public void update(Observable arg0, Object arg1) {
 		img = Controleur.getInstance().getImageModeleVue();
