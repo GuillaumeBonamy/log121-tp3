3c5af4c60e85716df2683a58f350528f059b1e40
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-07 20:25:33 +0000

    obs obj

--- trunk/log121/tp3/controleur/VueStatistiques.java
+++ trunk/log121/tp3/controleur/VueStatistiques.java
@@ -69,7 +69,7 @@ public class VueStatistiques extends JFrame implements Observer {
 	/**
 	 * 
 	 */
-	 public void update(Observable arg0, Object arg1) {
+	 public void update(Observable obs, Object obj) {
 		 img = Controleur.getInstance().getImageModeleVue();
 		 modifierStatistiques();
 	 }
