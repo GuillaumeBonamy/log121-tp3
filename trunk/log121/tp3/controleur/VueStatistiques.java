babf5c12ab696df9a8f23ccc6dfe316b8c8b498b
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-08 20:47:07 +0000

    J'ai modifier modifierStatistique pour que ça mette a jour la barre de zoom. En plus j'ai rajouté une méthode dont se sert vueModifie pour indiquer si le zoom augmente ou diminue.

--- trunk/log121/tp3/controleur/VueStatistiques.java
+++ trunk/log121/tp3/controleur/VueStatistiques.java
@@ -16,61 +16,92 @@ import log121.tp3.Image;
 
 
 public class VueStatistiques extends JFrame implements Observer {
-	
-	private JPanel panel;
-	private JTextField tf;
-	private JSlider slider;
-	private Image img;
-	
-	private void modifierStatistiques() {
-		tf.setText("x1:"+img.getX1()+ " x2: "+img.getX2()+"\ny1: "+img.getY1()+" y2: "+img.getY2());
-	}
-	
-	/**
-	 * Constructeur de la classe VueStatistiques
-	 * @param img L'image dont on doit afficher les statistiques
-	 */
-	public VueStatistiques()
-	{
-		setTitle("Vue statistique");
-		setLocation(850,450);
-		setSize(600,250);
-	
-		img = Controleur.getInstance().getImageModeleVue();
-		
-		panel=new JPanel(new GridLayout(1,2));
-		slider = ajouterSlider();
-		panel.add(slider);
-		tf=new JTextField("");
-		modifierStatistiques();
-		panel.add(tf);
-		
-		this.add(panel);
-	}
-	
-	private JSlider ajouterSlider()
-	{
-		slider = new JSlider(SwingConstants.HORIZONTAL,-100,100,0); // Debute a -100, finit a 100, valeur initiale a 0.
-		slider.setMajorTickSpacing(10); // Afficher les tirets tous les 10 pixels
-		slider.setPaintTicks(true); // Afficher les tirets
-		
-		Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
-		        
-		labelTable.put(new Integer( 0 ), new JLabel("Zoom normal") );         
-		labelTable.put(new Integer(-100), new JLabel("Zoom min") );               
-		labelTable.put(new Integer( 100 ),  new JLabel("Zoom max") );
-		                  
-		slider.setLabelTable(labelTable);
-		slider.setPaintLabels(true);
-		
-		return slider;
-	}
-	
-	/**
-	 * 
-	 */
-	 public void update(Observable obs, Object obj) {
-		 img = Controleur.getInstance().getImageModeleVue();
-		 modifierStatistiques();
-	 }
+        
+        private JPanel panel;
+        private JTextField tf;
+        private JSlider slider;
+        private Image img;
+        private int zoom=0;
+        private int zoomInit=0;
+        private int changement=-1;
+        
+        private void modifierStatistiques() {
+                tf.setText("x1:"+img.getX1()+ " x2: "+img.getX2()+"\ny1: "+img.getY1()+" y2: "+img.getY2());
+                
+                if(zoomInit == zoom)
+                	zoomInit++;
+                else if(changement == 1){
+                	zoom = zoom + 25;
+                	slider.setValue(zoom);
+                 
+                }
+                else if(changement == 0)
+                {
+                	
+                	
+                	if(zoom-25 >= 0)
+                	{
+                		zoom = zoom - 25;
+                		slider.setValue(zoom);
+                	}
+                }
+        }
+        
+
+        /**
+         * Constructeur de la classe VueStatistiques
+         * @param img L'image dont on doit afficher les statistiques
+         */
+        public VueStatistiques()
+        {
+                setTitle("Vue statistique");
+                setLocation(850,450);
+                setSize(600,250);
+        
+                img = Controleur.getInstance().getImageModeleVue();
+                
+                panel=new JPanel(new GridLayout(1,2));
+               
+                slider = ajouterSlider();
+                slider.setEnabled(false);
+                
+                tf=new JTextField("");
+               
+                modifierStatistiques();
+                
+                panel.add(slider);
+                panel.add(tf);
+                
+                this.add(panel);
+        }
+        
+        private JSlider ajouterSlider()
+        {
+                slider = new JSlider(SwingConstants.HORIZONTAL,0,325,0); // Debute a 0, finit a 325, valeur initiale a 0.
+                slider.setMajorTickSpacing(25); // Afficher les tirets tous les 25 pixels
+                slider.setPaintTicks(true); // Afficher les tirets
+                
+                Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+                        
+                labelTable.put(new Integer( 0 ), new JLabel("Zoom normal") );                     
+                labelTable.put(new Integer( 325 ),  new JLabel("Zoom max") );
+                                  
+                slider.setLabelTable(labelTable);
+                slider.setPaintLabels(true);
+                
+                return slider;
+        }
+        
+        public void setChangementSlider(int changement)
+        {
+        	this.changement=changement;
+        }
+        
+        /**
+         * 
+         */
+         public void update(Observable arg0, Object arg1) {
+                 img = Controleur.getInstance().getImageModeleVue();
+                 modifierStatistiques();
+         }
 }
