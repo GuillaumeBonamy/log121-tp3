18fd11a243d9645d94ec94fbe56c066ce8e22a8c
samuel.picard508@gmail.com <samuel.picard508@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-09 16:30:57 +0000

    Modification de commentaires et de menus

--- trunk/log121/tp3/controleur/VueImageModifie.java
+++ trunk/log121/tp3/controleur/VueImageModifie.java
@@ -110,11 +110,11 @@ public class VueImageModifie extends JFrame implements ActionListener,
                 annuler = new JMenuItem("Annuler");
                 imageInitiale = new JMenuItem("Image initiale");
                 translationBas = new JMenuItem("Translation en bas");
-                translationDroite = new JMenuItem("Translation Ãƒ  droite");
-                translationGauche = new JMenuItem("Translation Ãƒ  gauche");
+                translationDroite = new JMenuItem("Translation à droite");
+                translationGauche = new JMenuItem("Translation à gauche");
                 translationHaut = new JMenuItem("Translation en haut");
-                zoomIn = new JMenuItem("Zoom In");
-                zoomOut = new JMenuItem("Zoom Out");
+                zoomIn = new JMenuItem("Zoom Avant");
+                zoomOut = new JMenuItem("Zoom Arrière");
                 sauvegarder = new JMenuItem("Sauvegarder");
 
                 menuAction.add(imageInitiale);
