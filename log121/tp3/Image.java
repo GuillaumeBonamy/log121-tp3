86149e8b38e8a1a0224ac7fba0a997232dc51d47
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-09 03:07:44 +0000

    ajout de l'attribut zoom

--- log121/tp3/Image.java
+++ log121/tp3/Image.java
@@ -2,113 +2,125 @@ package log121.tp3;
 
 public class Image implements Cloneable {
 
-	private int x1, x2, y1, y2;
-	private int largeurOriginale, hauteurOriginale;
-	private String cheminImage;
+        private int x1, x2, y1, y2;
+        private int largeurOriginale, hauteurOriginale;
+        private String cheminImage;
+        private int zoom=0;
 
-	/**
-	 * Constructeur de la classe Image
-	 * 
-	 * @param cheminImage
-	 *            L'emplacement de l'image
-	 */
-	public Image(String cheminImage, int largeurOriginale, int hauteurOriginale) {
-		this.cheminImage = cheminImage;
-		this.largeurOriginale = largeurOriginale;
-		this.hauteurOriginale = hauteurOriginale;
-		this.x2 = largeurOriginale;
-		this.y2 = hauteurOriginale;
-	}
+        /**
+         * Constructeur de la classe Image
+         * 
+         * @param cheminImage
+         *            L'emplacement de l'image
+         */
+        public Image(String cheminImage, int largeurOriginale, int hauteurOriginale) {
+                this.cheminImage = cheminImage;
+                this.largeurOriginale = largeurOriginale;
+                this.hauteurOriginale = hauteurOriginale;
+                this.x2 = largeurOriginale;
+                this.y2 = hauteurOriginale;
+        }
 
-	/**
-	 * @return L'emplacement de l'image
-	 */
-	public String getCheminImage() {
-		return cheminImage;
-	}
+        /**
+         * @return le zoom de l'image
+         */
+        public int getZoom() { return zoom;}
+        
+        /**
+         * Modifie le zoom de l'image
+         * @param newZoom le nombre a ajouté pour avoir le nouveau zoom
+         */
+        public void setZoom(int newZoom) { this.zoom = this.zoom + newZoom; }
+        
+        /**
+         * @return L'emplacement de l'image
+         */
+        public String getCheminImage() {
+                return cheminImage;
+        }
 
-	/** @return La coordonnee x1 de l'image */
-	public int getX1() {
-		return x1;
-	}
+        /** @return La coordonnee x1 de l'image */
+        public int getX1() {
+                return x1;
+        }
 
-	/** @return La coordonnee y2 de l'image */
-	public int getY2() {
-		return y2;
-	}
+        /** @return La coordonnee y2 de l'image */
+        public int getY2() {
+                return y2;
+        }
 
-	/** @return La coordonnee x2 de l'image */
-	public int getX2() {
-		return x2;
-	}
+        /** @return La coordonnee x2 de l'image */
+        public int getX2() {
+                return x2;
+        }
 
-	/** @return La coordonnee y1 de l'image */
-	public int getY1() {
-		return y1;
-	}
-	
-	/** @return La largeur originale de l'image */
-	public int getLargeurOriginal() {
-		return largeurOriginale;
-	}
+        /** @return La coordonnee y1 de l'image */
+        public int getY1() {
+                return y1;
+        }
+        
+        /** @return La largeur originale de l'image */
+        public int getLargeurOriginal() {
+                return largeurOriginale;
+        }
 
-	/** @return La la heuteur originale de l'image */
-	public int getHauteurOriginale() {
-		return hauteurOriginale;
-	}
+        /** @return La la heuteur originale de l'image */
+        public int getHauteurOriginale() {
+                return hauteurOriginale;
+        }
 
-	/**
-	 * Modifie la coordonnee x1 de l'image
-	 * 
-	 * @param x1
-	 *            La nouvelle coordonnee
-	 */
-	public void setX1(int x1) {
-		this.x1 = x1;
-	}
+        /**
+         * Modifie la coordonnee x1 de l'image
+         * 
+         * @param x1
+         *            La nouvelle coordonnee
+         */
+        public void setX1(int x1) {
+                this.x1 = x1;
+        }
 
-	/**
-	 * Modifie la coordonnee x2 de l'image
-	 * 
-	 * @param x2
-	 *            La nouvelle coordonnee
-	 */
-	public void setX2(int x2) {
-		this.x2 = x2;
-	}
+        /**
+         * Modifie la coordonnee x2 de l'image
+         * 
+         * @param x2
+         *            La nouvelle coordonnee
+         */
+        public void setX2(int x2) {
+                this.x2 = x2;
+        }
 
-	/**
-	 * Modifie la coordonnee y1 de l'image
-	 * 
-	 * @param y1
-	 *            La nouvelle coordonnee
-	 */
-	public void setY1(int y1) {
-		this.y1 = y1;
-	}
+        /**
+         * Modifie la coordonnee y1 de l'image
+         * 
+         * @param y1
+         *            La nouvelle coordonnee
+         */
+        public void setY1(int y1) {
+                this.y1 = y1;
+        }
 
-	/**
-	 * Modifie la coordonnee y2 de l'image
-	 * 
-	 * @param y2
-	 *            La nouvelle coordonnee
-	 */
-	public void setY2(int y2) {
-		this.y2 = y2;
-	}
-	
-	/**
-	 * Cette fonction retourne un clone d'une image.
-	 * @return
-	 */
-	public Image getclone() {
-		Image i = null;
-		
-		try {
-			i = (Image) this.clone();
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-		}
-		return i;
-	}
+        /**
+         * Modifie la coordonnee y2 de l'image
+         * 
+         * @param y2
+         *            La nouvelle coordonnee
+         */
+        public void setY2(int y2) {
+                this.y2 = y2;
+        }
+        
+        /**
+         * Cette fonction retourne un clone d'une image.
+         * @return
+         */
+        public Image getclone() {
+                Image i = null;
+                
+                try {
+                        i = (Image) this.clone();
+                } catch (CloneNotSupportedException e) {
+                        e.printStackTrace();
+                }
+                return i;
+        }
 }
