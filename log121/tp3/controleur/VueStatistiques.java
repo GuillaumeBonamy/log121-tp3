103495789951e72c3feeeee73db06a8261b11d7f
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-09 05:18:06 +0000

    rajout de la méthode calculant le zoom a effectué sur l'image

--- log121/tp3/controleur/VueStatistiques.java
+++ log121/tp3/controleur/VueStatistiques.java
@@ -20,7 +20,9 @@ public class VueStatistiques extends JFrame implements Observer {
         private JPanel panel;
         private JTextField tf;
         private JSlider slider;
-        private Image img;
+        private Image img, imgTemp;
+        private final int FACTEUR_ZOOM=25;
+        private int compteurZoomEffectue=0;
         
         private void modifierStatistiques() {
                 tf.setText("x1:"+img.getX1()+ " x2: "+img.getX2()+"\ny1: "+img.getY1()+" y2: "+img.getY2());
@@ -57,14 +59,14 @@ public class VueStatistiques extends JFrame implements Observer {
         
         private JSlider ajouterSlider()
         {
-                slider = new JSlider(SwingConstants.HORIZONTAL,0,375,0); // Debute a 0, finit a 325, valeur initiale a 0.
+                slider = new JSlider(SwingConstants.HORIZONTAL,0,img.getNbZoom()*FACTEUR_ZOOM,0); // Debute a 0, finit a 325, valeur initiale a 0.
                 slider.setMajorTickSpacing(25); // Afficher les tirets tous les 25 pixels
                 slider.setPaintTicks(true); // Afficher les tirets
                 
                 Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
                         
                 labelTable.put(new Integer( 0 ), new JLabel("Zoom normal") );                     
-                labelTable.put(new Integer( 375 ),  new JLabel("Zoom max") );
+                labelTable.put(new Integer( img.getNbZoom()*FACTEUR_ZOOM ),  new JLabel("Zoom max") );
                                   
                 slider.setLabelTable(labelTable);
                 slider.setPaintLabels(true);
@@ -72,13 +74,56 @@ public class VueStatistiques extends JFrame implements Observer {
                 return slider;
         }
         
-
+        /**
+         * Methode servant à vérifier si l'image a été dézoomée ou zoomée
+         * @param imgTemp l'image précédente
+         * @param img l'image actuelle
+         */
+        public void verifierZoom(Image imgTemp, Image img)
+        {
+        	//si l'image est plus haute que large
+        	if(img.getLargeurOriginal() <= img.getHauteurOriginale()) {
+        		//si on zoom on incrémente le compteur de zoom
+	        	if(compteurZoomEffectue >= 0 &&compteurZoomEffectue < imgTemp.getNbZoom() && imgTemp.getY1() < img.getY1())
+	        	{
+	        		compteurZoomEffectue++;
+	        		
+	        	}
+	        	//si on dézoome on décrémente le compteur de zoom
+	        	else if(compteurZoomEffectue > 0 && compteurZoomEffectue <= imgTemp.getNbZoom() && imgTemp.getY1() > img.getY1())
+	        	{
+	        		compteurZoomEffectue--;
+	        	}
+        	}
+        	else {
+        		//si on zoome on incrémente le compteur de zoom
+        		if(compteurZoomEffectue >= 0 &&compteurZoomEffectue < imgTemp.getNbZoom() && imgTemp.getX1() < img.getX1())
+	        	{
+	        		compteurZoomEffectue++;
+	        		
+	        	}
+        		//si on dézoome on décrémente le compteur de zoom
+	        	else if(compteurZoomEffectue > 0 && compteurZoomEffectue <= imgTemp.getNbZoom() && imgTemp.getX1() > img.getX1())
+	        	{
+	        		compteurZoomEffectue--;
+	        	}
+        	}
+        		
+        	//on multiplie le compteur de zoom par le facteur de zoom, pour savoir quel est le zoom actuelle
+        	img.setZoom(compteurZoomEffectue*FACTEUR_ZOOM);
+        	//on modifie le nbZoom de l'image  actuel pour ne pas perdre la donnée
+        	img.setNbZoom(imgTemp.getNbZoom());	
+        	
+        	
+        }
         
         /**
          * 
          */
          public void update(Observable arg0, Object arg1) {
+        	 	 this.imgTemp=img;
                  img = Controleur.getInstance().getImageModeleVue();
+                 verifierZoom(imgTemp,img);
                  modifierStatistiques();
          }
 }
