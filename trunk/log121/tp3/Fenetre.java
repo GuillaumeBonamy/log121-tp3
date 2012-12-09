4c4db30fcb1b1762a441c2f32befffc82c57c544
bonamy.guillaume@gmail.com <bonamy.guillaume@gmail.com@9cebbd77-58da-6db2-3430-6d356af84990>
2012-12-09 03:10:09 +0000

    fini modif

--- trunk/log121/tp3/Fenetre.java
+++ trunk/log121/tp3/Fenetre.java
@@ -21,94 +21,94 @@ import log121.tp3.controleur.VueThumbnail;
 
 public class Fenetre extends JFrame implements ActionListener {
 
-	private JMenuBar menuBar;
-	private JMenu menuFichier;
-	private JMenuItem ouvrir, quitter;
+        private JMenuBar menuBar;
+        private JMenu menuFichier;
+        private JMenuItem ouvrir, quitter;
 
-	/**
-	 * Constructeur de la classe fenêtre
-	 */
-	public Fenetre() {
-		setTitle("TP3");
-		setLocation(0, 0);
-		setSize(400, 400);
+        /**
+         * Constructeur de la classe fenêtre
+         */
+        public Fenetre() {
+                setTitle("TP3");
+                setLocation(0, 0);
+                setSize(400, 400);
 
-		creerMenu();
+                creerMenu();
 
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent evt) {
-				System.exit(0);
-			}
-		});
+                addWindowListener(new WindowAdapter() {
+                        public void windowClosing(WindowEvent evt) {
+                                System.exit(0);
+                        }
+                });
 
-		setVisible(true);
+                setVisible(true);
 
-	}
+        }
 
-	/**
-	 * Methode permettant de creer le menu de la fenetre principale
-	 */
-	public void creerMenu() {
-		menuBar = new JMenuBar();
+        /**
+         * Methode permettant de creer le menu de la fenetre principale
+         */
+        public void creerMenu() {
+                menuBar = new JMenuBar();
 
-		menuFichier = new JMenu("Fichier");
-		ouvrir = new JMenuItem("Ouvrir");
-		quitter = new JMenuItem("Quitter");
+                menuFichier = new JMenu("Fichier");
+                ouvrir = new JMenuItem("Ouvrir");
+                quitter = new JMenuItem("Quitter");
 
-		menuFichier.add(ouvrir);
-		ouvrir.addActionListener(this);
-		ouvrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
-				ActionEvent.CTRL_MASK));
+                menuFichier.add(ouvrir);
+                ouvrir.addActionListener(this);
+                ouvrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
+                                ActionEvent.CTRL_MASK));
 
-		menuFichier.add(quitter);
-		quitter.addActionListener(this);
-		quitter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
-				ActionEvent.CTRL_MASK));
+                menuFichier.add(quitter);
+                quitter.addActionListener(this);
+                quitter.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
+                                ActionEvent.CTRL_MASK));
 
-		menuBar.add(menuFichier);
+                menuBar.add(menuFichier);
 
-		this.setJMenuBar(menuBar);
-	}
+                this.setJMenuBar(menuBar);
+        }
 
-	/**
-	 * Methode permettant de gerer les differentes actions des items
-	 */
-	public void actionPerformed(ActionEvent e) {
+        /**
+         * Methode permettant de gerer les differentes actions des items
+         */
+        public void actionPerformed(ActionEvent e) {
 
-		if (e.getSource() == ouvrir) {
-			ouvrir();
-		} else if (e.getSource() == quitter) {
-			System.exit(0);
-		}
-	}
+                if (e.getSource() == ouvrir) {
+                        ouvrir();
+                } else if (e.getSource() == quitter) {
+                        System.exit(0);
+                }
+        }
 
-	/**
-	 * Methode permettant d'ouvrir une image
-	 */
-	public void ouvrir() {
-		JFileChooser fc = new JFileChooser();
-		fc.setFileFilter(new FiltreImage());
-		fc.showOpenDialog(this);
-		if (fc.getSelectedFile() == null)
-			return;
-		ImageIcon iconeImage = new ImageIcon(fc.getSelectedFile()
-				.getAbsolutePath());
-		Controleur c = Controleur.getInstance();
+        /**
+         * Methode permettant d'ouvrir une image
+         */
+        public void ouvrir() {
+                JFileChooser fc = new JFileChooser();
+                fc.setFileFilter(new FiltreImage());
+                fc.showOpenDialog(this);
+                if (fc.getSelectedFile() == null)
+                        return;
+                ImageIcon iconeImage = new ImageIcon(fc.getSelectedFile()
+                                .getAbsolutePath());
+                Controleur c = Controleur.getInstance();
 
-		c.creerImageInitiale(fc.getSelectedFile().getAbsolutePath(),
-				iconeImage.getIconWidth(), iconeImage.getIconHeight());
+                c.creerImageInitiale(fc.getSelectedFile().getAbsolutePath(),
+                                iconeImage.getIconWidth(), iconeImage.getIconHeight());
 
-		VueThumbnail vt = new VueThumbnail();
-		VueStatistiques vs = new VueStatistiques();
-		VueImageModifie vim = new VueImageModifie(vs);
+                VueThumbnail vt = new VueThumbnail();
+                VueStatistiques vs = new VueStatistiques();
+                VueImageModifie vim = new VueImageModifie();
 
-		c.addObserverModelVue(vt);
-		c.addObserverModelVue(vim);
-		c.addObserverModelVue(vs);
+                c.addObserverModelVue(vt);
+                c.addObserverModelVue(vim);
+                c.addObserverModelVue(vs);
 
-		setVisible(true);
-		vt.setVisible(true);
-		vim.setVisible(true);
-		vs.setVisible(true);
-	}
+                setVisible(true);
+                vt.setVisible(true);
+                vim.setVisible(true);
+                vs.setVisible(true);
+        }
 }
